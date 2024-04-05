e688080a3f1a53630b4c147dd25cfae7d96237c5
Diego-Barreto-web <diegobarretopro@gmail.com>
2024-04-05 11:48:09 -0300

    Atualizações CSRF

--- ProjetoNetflixDjango/settings.py
+++ ProjetoNetflixDjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%aztn10nk8tnl-*!2*8_c#vts$@s+(x%#&3(3g6rhf5kcmerst'
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY=TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://netpi.up.railway.app/']
+else:
+    SECRET_KEY = 'django-insecure-%aztn10nk8tnl-*!2*8_c#vts$@s+(x%#&3(3g6rhf5kcmerst'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://netpi.up.railway.app/', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -88,7 +94,7 @@ DATABASES = {
 }
 
 import dj_database_url
-import os
+
 
 DATABASE_URL = 'postgresql://postgres:rgJOMsTBKfIQXcBkLXOCPsRVkQBzRzSX@viaduct.proxy.rlwy.net:52500/railway'
 if DATABASE_URL:
