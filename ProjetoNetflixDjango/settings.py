0c4927ed59afe489fd2ef417c8cd23a202b79e45
Diego-Barreto-web <diegobarretopro@gmail.com>
2024-04-05 12:58:37 -0300

    Alteração

--- ProjetoNetflixDjango/settings.py
+++ ProjetoNetflixDjango/settings.py
@@ -23,7 +23,7 @@ if TOKEN_CSRF:
 else:
     SECRET_KEY = 'django-insecure-%aztn10nk8tnl-*!2*8_c#vts$@s+(x%#&3(3g6rhf5kcmerst'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['netpi.up.railway.app', 'localhost', '127.0.0.1']
 
